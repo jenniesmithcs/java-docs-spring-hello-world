00fa7f82db71f83bcfca9cc2fea2a30f7bf8564d
Jennie Smith <jennie.s.smith@healthpartners.com>
2022-10-25 13:32:21 -0500

    Add logging

--- src/main/java/com/example/demo/DemoApplication.java
+++ src/main/java/com/example/demo/DemoApplication.java
@@ -1,10 +1,15 @@
 package com.example.demo;
 
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.time.ZoneId;
+import java.time.ZonedDateTime;
+
+@Slf4j
 @SpringBootApplication
 @RestController
 public class DemoApplication {
@@ -15,6 +20,7 @@ public class DemoApplication {
 
 	@RequestMapping("/")
 	String sayHello() {
+		log.info("Hitting endpoint at " + ZonedDateTime.now(ZoneId.of("America/Chicago")));
 		return "Hello Azure!";
 	}
 }
